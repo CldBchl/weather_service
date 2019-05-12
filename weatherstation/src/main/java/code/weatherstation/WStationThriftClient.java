c489856aa8b0ae994f662a158d5dca9351f6d1ce
ClaudiaB <claudiabc47@gmail.com>
2019-05-12 20:41:05 +0200

    #32 client login and logout

--- weatherstation/src/main/java/code/weatherstation/WStationThriftClient.java
+++ weatherstation/src/main/java/code/weatherstation/WStationThriftClient.java
@@ -3,67 +3,92 @@ package code.weatherstation;
 
 import code.weatherstation.thrift.Location;
 import code.weatherstation.thrift.Weather;
-import code.weatherstation.thrift.Weather.Client;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.apache.thrift.TException;
-import org.apache.thrift.protocol.TJSONProtocol;
-import org.apache.thrift.transport.TFramedTransport;
-import org.apache.thrift.transport.TSocket;
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.transport.THttpClient;
 import org.apache.thrift.transport.TTransportException;
 
 
 public class WStationThriftClient {
-  private static final Logger log = Logger.getLogger( WStationThriftClient.class.getName());
 
-  //TFramedTransport is needed with non-blocking servers
-  private TFramedTransport tFramedTransport;
+  private static final Logger log = Logger.getLogger(WStationThriftClient.class.getName());
+
+  //private TTransport transport;
+  private static THttpClient transport;
   private String stationName;
-  private TJSONProtocol protocol;
-  private Weather.Client weatherClient;
-  private static byte locationID=0; //manage locationIDs
+  private TBinaryProtocol protocol;
+  private static Weather.Client weatherClient;
+  private static byte locationId = 0x3;
   private Location location;
-  private long userId;
+  private static long userId;
 
-  public WStationThriftClient(String serverIP, String serverPort, String sName){
+  public WStationThriftClient(String serverIP, String serverPort, String sName) {
 
-    log.log(Level.INFO, "enter thrift client");
-    stationName=sName;
-    tFramedTransport= new TFramedTransport(new TSocket(serverIP,Integer.parseInt(serverPort)));
-    protocol= new TJSONProtocol(tFramedTransport);
-    weatherClient= new Client(protocol);
-    log.log(Level.INFO, "going on in  thrift client");
-
+    stationName = sName;
+    String url = "http://" + serverIP + ":" + serverPort + "/weather";
     try {
-      tFramedTransport.open();
+      transport = new THttpClient(url);
     } catch (TTransportException e) {
       e.printStackTrace();
     }
+    protocol = new TBinaryProtocol(transport);
+    weatherClient = new Weather.Client(protocol);
 
-    location= new Location(locationID,stationName, 23.23, 45.45);
-    log.log(Level.INFO, "after location");
+    //register shutDownTask which is executed when program exits
+    ShutDownTask shutDownTask = new ShutDownTask();
+    Runtime.getRuntime().addShutdownHook(shutDownTask);
 
-    try {
-      userId= weatherClient.login(location);
-      log.log(Level.INFO, "Login successful");
-      locationID++; //increase id for the next object
-    } catch (TException e) {
-      e.printStackTrace();
-      log.log(Level.WARNING, "Thrift-Login failed");
+    transport.open();
+    location = new Location(locationId, stationName, 23.24, 45.45);
+
+    Integer i = 0;
+    Boolean succesfulLogin = false;
+    while (!succesfulLogin) {
+      succesfulLogin = performLogin(weatherClient);
+      System.out.println("attempt " + i);
+      i++;
     }
-
-    try {
-      Boolean successfulLogout=weatherClient.logout(userId);
-      if (successfulLogout)
-        log.log(Level.INFO, "Login successful");
-    } catch (TException e) {
-      e.printStackTrace();
-      log.log(Level.WARNING, "Thrift-Logout failed");
-    }
-
-    tFramedTransport.close();
   }
 
+
+  private Boolean performLogin(Weather.Client client) {
+    try {
+      userId = client.login(location);
+      log.log(Level.INFO, "Login successful");
+      locationId++; //increase id for the next object
+      //return true;
+    } catch (TException e) {
+      e.printStackTrace();
+      log.log(Level.WARNING, "Error at login");
+      return false;
+    }
+    return true;
+  }
+
+
+
+
+
+  //private class which ensures a proper logout and closing of thrift after program exit
+  private static class ShutDownTask extends Thread {
+
+    @Override
+    public void run() {
+      System.out.println("Performing shutdown");
+      try {
+        Boolean successfulLogout = weatherClient.logout(userId);
+        if (successfulLogout) {
+          log.log(Level.INFO, "Logout successful");
+        }
+      } catch (TException e) {
+        e.printStackTrace();
+        log.log(Level.WARNING, "Error at login/logout");
+      }
+      transport.close();
+    }
+  }
 
 }
 
