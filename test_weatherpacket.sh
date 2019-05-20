7b9e6d2b1b042c3f0abaade9334df6d483498ed5
ClaudiaB <claudiabc47@gmail.com>
2019-05-20 20:34:22 +0200

    fix file not found error and update demo mode

--- test_weatherpacket.sh
+++ test_weatherpacket.sh
@@ -1,21 +1,23 @@
 #! /bin/bash
+# This file launches a weatherstation and four sensors, which send their data to the weatherstation
+# The sensors run in demo mode, this means that each one will only send five data records
 
 trap "kill 0" EXIT
 
-# params: type sourceIP sourcePort destIP destPort
+#remove files from previous test runs
+rm -r ./sensorData/Demo1
 
 echo "Starting Weatherstations:"
 # parameters Stationname IpForSensors PortForSensors IpForHttp PortForHttp locationID IpForThriftServer PortForThriftServer
-rm -r ./sensorData/Demo1
-./weatherstation/build/install/weatherstation/bin/weatherstation Demo1  0.0.0.0 5555 0.0.0.0 5554 2 0.0.0.0 9090 &
+./weatherstation/build/install/weatherstation/bin/weatherstation Demo1  127.0.0.1 5555 127.0.0.1 5554 0 0.0.0.0 9090 &
 
 
 echo "Starting Sensors:"
 # params: type interval sourceIP sourcePort destIP destPort
 
-./sensor/build/install/sensor/bin/sensor temperature 2 0.0.0.0 5556 0.0.0.0 5555 demo &
-./sensor/build/install/sensor/bin/sensor humidity 2 0.0.0.0 5557 0.0.0.0 5555 demo &
-./sensor/build/install/sensor/bin/sensor wind 2 0.0.0.0 5558 0.0.0.0 5555 demo &
-./sensor/build/install/sensor/bin/sensor rain 2 0.0.0.0 5559 0.0.0.0 5555 demo&
+./sensor/build/install/sensor/bin/sensor temperature 2 127.0.0.1 5556 127.0.0.1 5555 demo &
+./sensor/build/install/sensor/bin/sensor humidity 2 127.0.0.1 5557 127.0.0.1 5555 demo &
+./sensor/build/install/sensor/bin/sensor wind 2 127.0.0.1 5558 127.0.0.1 5555 demo &
+./sensor/build/install/sensor/bin/sensor rain 2 127.0.0.1 5559 127.0.0.1 5555 demo&
 
 wait
